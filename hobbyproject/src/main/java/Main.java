b295f3893c7f3111e7cc0fd45acef58453d985d3
HanniSalman <hannisalman2510@outlook.com>
2024-02-26 10:22:31 +0100

    startup

---
+++ hobbyproject/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig();
+    }
+}
