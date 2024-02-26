b824f82287ad725afc0dd35f5bbe48e47b775dbf
lassekh <31202809+lassekh@users.noreply.github.com>
2024-02-26 17:09:38 +0100

    US-3 completed - NOT tested

--- hobbyproject/src/main/java/dao/HobbyDAO.java
+++ hobbyproject/src/main/java/dao/HobbyDAO.java
@@ -1,4 +1,32 @@
 package dao;
 
-public class HobbyDAO extends CRUDDao{
+import entities.Account;
+import entities.Hobby;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class HobbyDAO extends CRUDDao {
+    private static EntityManagerFactory emf;
+    private static HobbyDAO instance;
+    public static HobbyDAO getInstance(EntityManagerFactory _emf)
+    {
+        if(instance == null)
+        {
+            emf = _emf;
+            instance = new HobbyDAO();
+        }
+        return instance;
+    }
+    public List<Account> getAllAccountsWithHobby(Hobby hobby)
+    {
+        try(EntityManager em = emf.createEntityManager())
+        {
+            TypedQuery<Account> query = em.createQuery("select a from Account a join Hobby h where h.id = ?1", Account.class);
+            query.setParameter(1,hobby.getId());
+            return query.getResultList();
+        }
+    }
 }
