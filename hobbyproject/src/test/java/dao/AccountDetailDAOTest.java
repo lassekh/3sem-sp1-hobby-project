1c3402f399cbce30bcf6b1d4a208c8ca7912c519
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-02-27 17:38:24 +0100

    Finished

--- hobbyproject/src/test/java/dao/AccountDetailDAOTest.java
+++ hobbyproject/src/test/java/dao/AccountDetailDAOTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -40,14 +41,11 @@ class AccountDetailDAOTest {
         Hobby hobby1 = new Hobby("Volleyball", "wiki@test.dk", "General", Hobby.Type.INDOOR);
         Hobby hobby2 = new Hobby("Fodbold", "wiki@test.dk", "General", Hobby.Type.OUTDOOR);
 
-        //em.persist(hobby1);
-        //em.persist(hobby2);
-
         Account accountAhmad = new Account("Ahmad A");
         accountAhmad.addHobby(hobby1);
         accountAhmad.addHobby(hobby2);
 
-        AccountDetail accountDetailAhmad = new AccountDetail(123, LocalDate.of(1998, 3, 6), "Boulevard");
+        AccountDetail accountDetailAhmad = new AccountDetail(123, 456, LocalDate.of(1998, 3, 6), "Boulevard");
         accountDetailAhmad.setCity(city);
         accountAhmad.addAccountDetail(accountDetailAhmad);
 
@@ -66,7 +64,7 @@ class AccountDetailDAOTest {
     }
 
     @Test
-    @DisplayName("Getting all information from a given person using id")
+    @DisplayName("Getting all information from a given person using id.")
     void getAllInformationFromAGivenPersonUsingId() {
         // Given
         String expectedFullName = "Ahmad A";
@@ -79,5 +77,59 @@ class AccountDetailDAOTest {
         assertNotNull(accountDTO);
         assertEquals(expectedFullName, accountDTO.getFullName());
         assertEquals(expectedHobbySize, accountDTO.getHobbies().size());
+    }
+
+    @Test
+    @DisplayName("Getting all phone numbers from a given person.")
+    void getAllPhoneNumbersFromGivenPerson() {
+        // Given
+        int expectedNumbersSize = 2;
+
+        // When
+        String setName = "Ahmad A";
+        List<Integer> actualNumbers = accountDetailDAO.getAllPhoneNumbersFromGivenPersonByName(setName);
+
+        // Then
+        assertNotNull(actualNumbers);
+        assertEquals(expectedNumbersSize, actualNumbers.size());
+    }
+
+    @Test
+    @DisplayName("Getting all persons in a specific city by zipcode.")
+    void getPersonsInACityByZipcode() {
+        // Given
+        int expectedPersonsSize = 1;
+        int expectedPrivateNumber = 123;
+        int expectedWorkNumber = 456;
+        int expectedZipcode = 2800;
+
+        // When
+        int setZipcode = 2800;
+        List<Account> actualAccountList = accountDetailDAO.getPersonsInASpecifikCityByZipcode(setZipcode);
+
+        // Then
+        assertNotNull(actualAccountList);
+        assertEquals(expectedPersonsSize, actualAccountList.size());
+        assertEquals(expectedZipcode, actualAccountList.get(0).getAccountDetail().getZipcode());
+        assertEquals(expectedPrivateNumber, actualAccountList.get(0).getAccountDetail().getPrivateMobile());
+        assertEquals(expectedWorkNumber, actualAccountList.get(0).getAccountDetail().getWorkMobile());
+    }
+
+    @Test
+    @DisplayName("Getting all persons in a specific city by name.")
+    void getPersonsInASpecifikCityByName() {
+        // Given
+        int expectedPersonsSize = 1;
+        int expectedZipCode = 2800; // Since we can't get hold of the city name, we'll use the corresponding zipcode
+
+        // When
+        String setCityName = "Kongens Lyngby";
+        List<Account> actualAccountList = accountDetailDAO.getPersonsInASpecifikCityByName(setCityName);
+
+
+        // Then
+        assertNotNull(actualAccountList);
+        assertEquals(expectedPersonsSize, actualAccountList.size());
+        assertEquals(expectedZipCode,actualAccountList.get(0).getAccountDetail().getZipcode());
     }
 }
