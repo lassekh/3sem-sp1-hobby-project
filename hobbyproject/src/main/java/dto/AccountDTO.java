7787957eef87d12574c908ad0c2097d58d978ad7
lassekh <31202809+lassekh@users.noreply.github.com>
2024-02-27 10:13:12 +0100

    edited

--- hobbyproject/src/main/java/dto/AccountDTO.java
+++ hobbyproject/src/main/java/dto/AccountDTO.java
@@ -23,20 +23,4 @@ public class AccountDTO {
     private String cityName;
     private String address;
     private Set<Hobby> hobbies;
-    private String hobbyName;
-    private String category;
-    private String type;
-
-    public AccountDTO(int id, String fullName, LocalDate dateOfBirth, int mobile, LocalDate updatedAt, int zipcode, String cityName, String address, String hobbyName, String category) {
-        this.id = id;
-        this.fullName = fullName;
-        this.dateOfBirth = dateOfBirth;
-        this.mobile = mobile;
-        this.updatedAt = updatedAt;
-        this.zipcode = zipcode;
-        this.cityName = cityName;
-        this.address = address;
-        this.hobbyName = hobbyName;
-        this.category = category;
-    }
 }
